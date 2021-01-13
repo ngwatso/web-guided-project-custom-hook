185227e7179620f0847bb5a62016afb685e5ea75
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-13 13:56:33 -0500

    adds to lecture.

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const initialValues = {
-  firstName:"",
+  firstName: "",
   email: ""
 };
 
--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -1,7 +1,30 @@
 import { useState } from 'react';
 
+const useLocalStorage = (key, initialValue) => {
+    //1. When we create our state, look inside localStorage to see if it already exists.
+    //2. If it does exist, put that saved value into state. If not put in initialValue.
+    //3. When we modify our state, save that change to localStorage as well.
+
+    const [storedValue, setStoredValue] = useState(()=>{
+        if (localStorage.getItem(key)) {
+            return JSON.parse(localStorage.getItem(key));
+        }
+
+        localStorage.setItem(key, initialValue);
+        return initialValue;
+    });
+
+    const setValue = value => {
+        setStoredValue(value);
+        localStorage.setItem(key, value);
+    }
+
+    return [storedValue, setValue];
+}
+
+
 const useForm = (initialValues)=> {
-    const [values, setValues] = useState(initialValues);
+    const [values, setValues] = useLocalStorage("form", initialValues);
   
     const handleChanges = e => {
       //setValues("warren")
