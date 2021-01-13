2e6ad2381b9ab7fc8ceac8f26d128c947076c8a0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-13 12:44:39 -0500

    lecture work-in-progress

---
+++ review/src/hooks/pokemonHooks.js
@@ -0,0 +1,18 @@
+const usePokeState = (initialValues) => {
+    const [pokemen, setPokemen] = useState(initialValues.pokemen);
+    const [selectedPokemon, setSelectedPokemon] = useState(initialValues.selectedPokemon);
+
+    useEffect(() => {
+        setPokemen(pokemonServices.fetchAllPoke());
+    }, []);
+
+    const handlePoke = (id) => {
+        pokemonServices.fetchSelectedPoke(id).then((data) => {
+            setSelectedPokemon(data);
+        });
+    };
+
+    return([pokemen, selectedPokemon, handlePoke]);
+}
+
+export default usePokeState;
