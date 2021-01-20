87418295cc3991027f31c83a2b743138ddcc1d36
ngwatso <ngwatson79@yahoo.com>
2021-01-20 14:01:40 -0500

    progress 3

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -1,7 +1,32 @@
 import { useState } from "react";
 
+const useLocalStorage = (key, initialValue) => {
+	// ? 1. When we create our state, look inside localStorage to see if it already exists.
+	// ? 2. If it does exist, put that saved value into state.  If not, put in initialValue.
+	// ? 3. When we modify our state, save that change to localStorage as well.
+
+	const [storedValue, setStoredValue] = useState(() => {
+		if (localStorage.getItem(key)) {
+			return JSON.parse(localStorage.getItem(key));
+		}
+
+		localStorage.setItem(key, initialValue);
+
+		return initialValue;
+	});
+
+	const setValue = (value) => {
+		setStoredValue(value);
+		localStorage.setItem(key, value);
+	};
+
+	return [storedValue, setValue];
+};
+
+// ? Composing hooks - putting a hook inside of a hook
+
 const useForm = (initialValue) => {
-	const [values, setValues] = useState(initialValue);
+	const [values, setValues] = useLocalStorage("form", initialValues);
 
 	const handleChanges = (e) => {
 		setValues({
